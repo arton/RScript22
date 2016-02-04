069c0442949d8ab8889888839fd1917d90ea46fe
arton <artonx@gmail.com>
2016-02-05 01:12:09 +0900

    support imported object that has capitalized name

--- RScript22/lib/activescriptruby.rb
+++ RScript22/lib/activescriptruby.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 #
-# Copyright(c) 2004 arton
+# Copyright(c) 2004,2015,2016 arton
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,7 @@ require 'win32ole'
 require 'thread'
 require 'set'
 
-module ActiveScriptRuby 
+module ActiveScriptRuby
 
   class AsrProxy
     USER_DISPID = 28000
@@ -40,7 +40,7 @@ module ActiveScriptRuby
       @target.to_enum
     end
     def add_method(name, script, fn, line)
-      define_singleton_method(name.to_sym) { 
+      define_singleton_method(name.to_sym) {
         instance_eval(script, fn, line)
       }
     end
@@ -62,7 +62,7 @@ module ActiveScriptRuby
       val
     end
   end
-    
+
   class SelfProxy < AsrProxy
     def initialize
       super(self)
@@ -71,12 +71,12 @@ module ActiveScriptRuby
 
   class GlobalProxy < AsrProxy
     @lock = Mutex.new
-    
+
     NamedItem = Struct.new(:obj, :global) do
       def eql? (other) obj.eql?(other.obj); end
       def hash; obj.hash; end
     end
-    
+
     def initialize(bridge)
       super(self)
       @bridge = bridge
@@ -87,11 +87,11 @@ module ActiveScriptRuby
     def to_variant(obj)
       @bridge.PassObject(obj)
     end
-    
+
     def self_to_variant
       @bridge.PassObject(self)
     end
-    
+
     def to_proxy(val)
       obj = AsrProxy.new(val)
       @bridge.PassObject(obj)
@@ -101,9 +101,9 @@ module ActiveScriptRuby
     def ruby_version
       "#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL} (#{RUBY_PLATFORM})"
     end
-    
+
     def create_event_handler(itemname, procname, code, line)
-      val = @named_items[itemname] 
+      val = @named_items[itemname]
       unless val
         obj = SelfProxy.new
         add_named_item(itemname, obj, false)
@@ -117,9 +117,9 @@ module ActiveScriptRuby
       obj.add_method(procname, code, '(asr)', line)
       obj
     end
-    
+
     def create_event_proc(itemname, code, line)
-      val = @named_items[itemname] 
+      val = @named_items[itemname]
       unless val
         obj = SelfProxy.new
         add_named_item(itemname, obj, false)
@@ -134,38 +134,52 @@ module ActiveScriptRuby
         obj.instance_eval(code, '(asr)', line)
       end
     end
-    
+
     def method_missing(id, *args)
       @global.__send__(id, *args) if @global
     end
-    
+
     def add_named_item(name, obj, global)
       @named_items[name] = NamedItem.new(obj, global)
-      define_singleton_method(name.to_sym) { obj }
+      if name[0].between?('A', 'Z')
+        GlobalProxy.const_set(name, obj)
+      else
+        define_singleton_method(name.to_sym) { obj }
+      end
       if global
         @global = obj
-        # ASR 1.0 compatibility
-        instance_eval("#{name.capitalize} = #{name}; @#{name} = #{name}")
       end
+      # ASR 1.0 compatibility
+      instance_eval("#{name.capitalize} = #{name}; @#{name} = #{name}")
     end
-    
+
     def add_constant(name, variant)
       define_singleton_method(name.to_sym) { variant.value }
     end
-    
+
     def remove_item(name)
       val = @named_items[name]
       if val
-        define_singleton_method(name.to_sym) { nil }
+        if name[0].between?('A', 'Z')
+          GlobalProxy.delete_constant(name.to_sym)
+        else
+          define_singleton_method(name.to_sym) { nil }
+        end
         val.obj.ole_free if WIN32OLE === val.obj
         if val.global
-          # ASR 1.0 compatibility
-          instance_eval("#{name.capitalize} = @#{name} = nil")
+          @global = nil
         end
+        # ASR 1.0 compatibility
+        instance_eval("@#{name} = nil")
+        remove_const(name.capitalize)
         @named_items[name] = nil
       end
     end
-    
+
+    def self.delete_constant(name)
+      remove_const(name)
+    end
+
     def remove_items
       @named_items.each { |key, val| remove_item(key) if val }
       @named_items.clear
